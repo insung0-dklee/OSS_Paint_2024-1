685b0a4f7e317797ad2669a70ba0dc4b8b98d5b1
Junseok Oh <breqd10119@gmail.com>
2024-05-30 18:13:12 +0900

    [기능 추가] 캔버스의 배경색을 바꾸는 버튼 추가

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from tkinter import *
+import random
 
 def paint(event):
     x1, y1 = ( event.x-1 ), ( event.y-1 )
     x2, y2 = ( event.x+1 ), ( event.y+1 )
     canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
 
+def change_canvas_color():
+    colors = ["red", "green", "blue", "yellow", "pink", "purple", "orange"]
+    canvas.configure(bg=random.choice(colors))
+
 window = Tk()
-canvas = Canvas(window)
+window.title("Paint and Change Canvas Color")
+
+canvas = Canvas(window, width=400, height=400)
 canvas.pack()
+
 canvas.bind("<B1-Motion>", paint)
+
+color_button = Button(window, text="Random Canvas Color", command=change_canvas_color)
+color_button.pack()
+
 window.mainloop()
