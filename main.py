390f5db4f66df16dd9c527482653a19dbd5cdaba
dkduslee <sw56eet@gmail.com>
2024-05-30 23:34:47 +0900

    [기능추가] 원 도형을 만드는 기능 추가

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ clear_paint : 그림판에 있는 그림을 다 지우는 기능
 button_delete : clear_paint의 버튼
 
 """
-
+import tkinter as tk
 from tkinter import *
 
 def paint(event):
@@ -26,3 +26,23 @@ button_delete = Button(window, text="all clear", command=clear_paint)
 button_delete.pack()
 
 window.mainloop()
+
+#원 도형을 그리는 기능 추가
+def Circle(self, root):
+        self.root = root
+        self.root.title("그림판")
+
+        self.current_tool = "oval"
+        self.color = "black"
+        self.start_x = None
+        self.start_y = None
+        self.oval_id = None
+
+        self.canvas = tk.Canvas(root, bg="white", width=800, height=600)
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.canvas.bind("<Button-1>", self.on_click)
+        self.canvas.bind("<B1-Motion>", self.on_drag)
+        self.canvas.bind("<ButtonRelease-1>", self.on_release)
+
+        self.setup_tools()
+
