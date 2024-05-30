235b2cc701428a973f285d8239f5ab8ca942d7bc
JaeYoon Lee <fkdpse@naver.com>
2024-05-30 19:34:33 +0900

    [기능추가]드로퍼(스포이드)기능 추가#실행을 위해서Pillow 인스톨 필요

--- main.py
+++ main.py
@@ -7,22 +7,39 @@ button_delete : clear_paint의 버튼
 """
 
 from tkinter import *
+from PIL import ImageGrab #pip install Pillow필요
 
 def paint(event):
-    x1, y1 = ( event.x-1 ), ( event.y-1 )
-    x2, y2 = ( event.x+1 ), ( event.y+1 )
-    canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
+    global selected_color
+    x1, y1 = (event.x-1), (event.y-1)
+    x2, y2 = (event.x+1), (event.y+1)
+    canvas.create_oval(x1, y1, x2, y2, fill=selected_color, outline=selected_color)#원본 검정에서 선택된 색으로 칠하도록 변경
 
-#all clear 기능 추가
 def clear_paint():
     canvas.delete("all")
 
+def activate_dropper():
+    canvas.bind("<Button-1>", get_pixel_color)
+
+def get_pixel_color(event):
+    global selected_color
+    x, y = canvas.winfo_rootx() + event.x, canvas.winfo_rooty() + event.y
+    image = ImageGrab.grab(bbox=(x, y, x+1, y+1))
+    selected_color = "#" + "".join([f"{i:02x}" for i in image.getpixel((0, 0))])  # RGB를 hex 코드로 변환
+    canvas.bind("<Button-1>", lambda event: None)  # 스포이드 모드 비활성화
+    canvas.bind("<B1-Motion>", paint)  # 다시 그리기 모드로 전환
+
 window = Tk()
+selected_color = "#000000" # 초기 색상은 검정색
+
 canvas = Canvas(window)
-canvas.pack()
+canvas.pack(fill=BOTH, expand=True)
 canvas.bind("<B1-Motion>", paint)
 
 button_delete = Button(window, text="all clear", command=clear_paint)
-button_delete.pack()
+button_delete.pack(side=LEFT)
+
+dropper_button = Button(window, text="dropper", command=activate_dropper)
+dropper_button.pack(side=RIGHT)
 
 window.mainloop()
