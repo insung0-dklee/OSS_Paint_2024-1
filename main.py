205ead42622156e85ea8ef8433ca708fd0f881cf
sihyeong LEE <tlgud119@gamil.com>
2024-05-30 19:48:23 +0900

    [기능추가]  축소 및 확대  함수 구현  모듈로 구현하고 main에서 호출

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ Project : Paint
 paint : 내외부 검은색의 2픽셀 크기의 원을 이용해 그림을 그리는 기능
 clear_paint : 그림판에 있는 그림을 다 지우는 기능
 button_delete : clear_paint의 버튼
-
 """
 
 from tkinter import *
+from zoom import zoom
 
 def paint(event):
-    x1, y1 = ( event.x-1 ), ( event.y-1 )
-    x2, y2 = ( event.x+1 ), ( event.y+1 )
+    x1, y1 = (event.x-1), (event.y-1)
+    x2, y2 = (event.x+1), (event.y+1)
     canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
 
-#all clear 기능 추가
+# all clear 기능 추가
 def clear_paint():
     canvas.delete("all")
+
+# 확대 축소 기능
+def zoom_in(event=None):
+    zoom(canvas, 1)
+
+def zoom_out(event=None):
+    zoom(canvas, -1)
 
 window = Tk()
 canvas = Canvas(window)
@@ -24,5 +31,16 @@ canvas.bind("<B1-Motion>", paint)
 
 button_delete = Button(window, text="all clear", command=clear_paint)
 button_delete.pack()
+
+# 확대 Button : event->zoom_in
+btn_zoom_in = Button(window, text="확대", command=zoom_in)
+btn_zoom_in.pack(side=LEFT)
+
+# 축소 버튼 : event->zoom_out
+btn_zoom_out = Button(window, text="축소", command=zoom_out)
+btn_zoom_out.pack(side=LEFT)
+
+# 마우스 휠 이벤트에 확대 / 축소 함수 바인딩
+canvas.bind("<MouseWheel>", lambda event: zoom(canvas, event.delta))
 
 window.mainloop()
---
+++ zoom.py
@@ -0,0 +1,29 @@
+def zoom(canvas, scale_factor):
+    scale = 1.0
+    if scale_factor > 0:
+        scale *= 1.1
+    elif scale_factor < 0:
+        scale /= 1.1
+
+    # 캔버스 중심점을 기준으로 확대 / 축소
+    canvas_scale_x = canvas.winfo_width() / 2
+    canvas_scale_y = canvas.winfo_height() / 2
+
+    canvas.scale("all", canvas_scale_x, canvas_scale_y, scale, scale)
+
+    """
+    확대 및 축소 함수
+    @Zoom Param
+    event: Tkinter에서 발생하는 이벤트를 나타내는 객체
+    scale_factor : 확대일 경우 1을 축소일 경우 - 1을 인수로 받음
+
+    @fun
+    enlargement event : 기존 scale * 1.1
+    reduction evnet : 기존 scale / 1.1
+    canvas_scale_x : Get the width of the canvas and divide by 2
+    canvas_scale_y : Get the height of the canvas and divide by 2
+    canvas.scale : Change scale size using x and y coordinates
+    @Return
+    None
+
+    """
