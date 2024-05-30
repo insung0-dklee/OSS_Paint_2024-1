8bcdf45b0fa3aa18f04a50372ec2bb46f696bb3d
WonChang Oh <owch0731@gmail.com>
2024-05-30 19:28:50 +0900

    그림판에 Ctrl+Z 를 눌러 되돌리기 기능추가

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from tkinter import *
 
 def paint(event):
-    x1, y1 = ( event.x-1 ), ( event.y-1 )
-    x2, y2 = ( event.x+1 ), ( event.y+1 )
-    canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
+    x1, y1 = (event.x - 1), (event.y - 1)
+    x2, y2 = (event.x + 1), (event.y + 1)
+    dot = canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
+    # 사용자가 그린 도형의 ID를 actions 리스트에 추가
+    actions.append(dot)
+
+def undo(event):
+    if actions:
+        dot_id = actions.pop()  # 마지막 도형의 ID를 가져옴
+        canvas.delete(dot_id)  # 해당 도형 삭제
 
 window = Tk()
+
+actions = []  # 사용자의 행동을 저장할 리스트
+
 canvas = Canvas(window)
 canvas.pack()
 canvas.bind("<B1-Motion>", paint)
+
+# Ctrl+Z를 누를 경우 undo 함수 호출
+window.bind("<Control-z>", undo)
+
 window.mainloop()
