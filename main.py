2a28a84a325a393fc5212b1902ca489f6038ed57
KangHyun Lee <singa0327@yu.ac.kr>
2024-05-30 20:26:27 +0900

    [기능추가] (100,100)좌표에 입력받은 반지름의 원 그리기 기능 추가

--- main.py
+++ main.py
@@ -3,26 +3,41 @@ Project : Paint
 paint : 내외부 검은색의 2픽셀 크기의 원을 이용해 그림을 그리는 기능
 clear_paint : 그림판에 있는 그림을 다 지우는 기능
 button_delete : clear_paint의 버튼
-
 """
 
 from tkinter import *
 
 def paint(event):
-    x1, y1 = ( event.x-1 ), ( event.y-1 )
-    x2, y2 = ( event.x+1 ), ( event.y+1 )
+    x1, y1 = (event.x-1), (event.y-1)
+    x2, y2 = (event.x+1), (event.y+1)
     canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
 
-#all clear 기능 추가
 def clear_paint():
     canvas.delete("all")
 
+#(100,100)좌표에 원을 그리는 함수
+#radius가 98을 넘을시 원이 canvas의 크기를 넘게 된다.
+def circle_radius():
+    def draw_circle():
+        radius = int(radius_Entry.get())
+        Draw_Circle(canvas, 100, 100, radius)
+    lab_Circle = Label(window, text="Radius(0<radius<98) = "
+    lab_Circle.grid(row=1, column=2)
+    radius_Entry = Entry(bg='white', width=8)
+    radius_Entry.grid(row=1, column=3)
+    button_confirm = Button(window, text="Confirm", command=draw_circle)
+    button_confirm.grid(row=1, column=4)
+
+def Draw_Circle(canvas, x, y, r):
+    canvas.create_oval(x-r, y-r, x+r, y+r)
+
 window = Tk()
 canvas = Canvas(window)
-canvas.pack()
+canvas.grid(row=2, column=0)
 canvas.bind("<B1-Motion>", paint)
-
 button_delete = Button(window, text="all clear", command=clear_paint)
-button_delete.pack()
+button_delete.grid(row=0, column=1)
+button_oval = Button(window, text="Draw Oval", command=circle_radius)
+button_oval.grid(row=1, column=1)
 
 window.mainloop()
