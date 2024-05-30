abeeef447e2e4314860cf79e8dc43c89bf13c01d
Jun Kim <junkim8193@gmail.com>
2024-05-30 19:00:10 +0900

    [기능 추가] 그림판 작업물 좌우 반전을 시켜주는 버튼을 추가하였습니다.

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ button_delete : clear_paint의 버튼
 from tkinter import *
 
 def paint(event):
-    x1, y1 = ( event.x-1 ), ( event.y-1 )
-    x2, y2 = ( event.x+1 ), ( event.y+1 )
+    x1, y1 = (event.x - 1), (event.y - 1)
+    x2, y2 = (event.x + 1), (event.y + 1)
     canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
 
-#all clear 기능 추가
+# all clear 기능 추가
 def clear_paint():
     canvas.delete("all")
+
+# 좌우 반전 기능 추가
+def flip_horizontal():
+    objects = canvas.find_all()
+    canvas.update()
+    canvas_width = canvas.winfo_width()
+    for obj in objects:
+        coords = canvas.coords(obj)
+        for i in range(len(coords)):
+            if i % 2 == 0:  # x 좌표를 반전시킵니다.
+                coords[i] = canvas_width - coords[i]
+        canvas.coords(obj, *coords)
 
 window = Tk()
 canvas = Canvas(window)
@@ -24,5 +36,8 @@ canvas.bind("<B1-Motion>", paint)
 
 button_delete = Button(window, text="all clear", command=clear_paint)
 button_delete.pack()
+
+button_flip = Button(window, text="Flip Horizontal", command=flip_horizontal)
+button_flip.pack()
 
 window.mainloop()
