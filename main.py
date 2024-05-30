cce6a776d70aa5186c64de4e57a20f5f33f36ffd
HJ <104305435+w8err@users.noreply.github.com>
2024-05-30 19:20:29 +0900

    [Fix] main.py 수정

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
+"""
+Project : Paint
+paint : 내외부 검은색의 2픽셀 크기의 원을 이용해 그림을 그리는 기능
+clear_paint : 그림판에 있는 그림을 다 지우는 기능
+button_delete : clear_paint의 버튼
+
+"""
+
 from tkinter import *
-from erase import erase
-import thickness_ctrl as tc  # 선 굵기 조절 기능을 담은 파일 import
 
 def paint(event):
-    x1, y1 = (event.x - tc.line_thickness), (event.y - tc.line_thickness)
-    x2, y2 = (event.x + tc.line_thickness), (event.y + tc.line_thickness)
+    x1, y1 = ( event.x-1 ), ( event.y-1 )
+    x2, y2 = ( event.x+1 ), ( event.y+1 )
     canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
+
+#all clear 기능 추가
+def clear_paint():
+    canvas.delete("all")
 
 window = Tk()
 canvas = Canvas(window)
 canvas.pack()
-
 canvas.bind("<B1-Motion>", paint)
-canvas.bind("<B3-Motion>", lambda event: erase(event, canvas))
 
-# 선 굵기 조절 버튼 추가
-increase_button = Button(window, text="+", command=tc.increase_thickness)
-decrease_button = Button(window, text="-", command=tc.decrease_thickness)
-
-increase_button.pack(side=LEFT)
-decrease_button.pack(side=LEFT)
+button_delete = Button(window, text="all clear", command=clear_paint)
+button_delete.pack()
 
 window.mainloop()
