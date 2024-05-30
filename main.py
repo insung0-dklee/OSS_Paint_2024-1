16fb52d5bb4c5046a93c19d2967c578cbb393f2d
Minji Kim <eegnim@naver.com>
2024-05-30 18:37:45 +0900

    [기능 추가] 그림판의 사용자가 캔버스 위에 그린 모든 것(그림, 글씨, 배경 등)들을 지우는 기능 추가.

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ def paint(event):
     x2, y2 = ( event.x+1 ), ( event.y+1 )
     canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
 
+def clear_canvas():
+    canvas.delete("all")
+
 window = Tk()
 canvas = Canvas(window)
 canvas.pack()
 canvas.bind("<B1-Motion>", paint)
 window.mainloop()
+clear = Button(window, text="모두 지우기", command=clear_canvas)
+clear.pack()
