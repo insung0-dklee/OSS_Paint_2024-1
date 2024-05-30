987a89721b1d10e354764e0bba0f722dc170937a
DESKTOP-NRMGF8I\조수현 <plm9697@naver.com>
2024-05-31 01:44:06 +0900

    [기능추가] 마우스 우클릭으로 선 그리기 추가

--- main.py
+++ main.py
@@ -9,20 +9,28 @@ button_delete : clear_paint의 버튼
 from tkinter import *
 
 def paint(event):
-    x1, y1 = ( event.x-1 ), ( event.y-1 )
-    x2, y2 = ( event.x+1 ), ( event.y+1 )
+    x1, y1 = (event.x-1), (event.y-1)
+    x2, y2 = (event.x+1), (event.y+1)
     canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
 
-#all clear 기능 추가
 def clear_paint():
     canvas.delete("all")
+
+def draw_line(event):
+    if not hasattr(draw_line, 'prev_x') or not hasattr(draw_line, 'prev_y'):
+        draw_line.prev_x, draw_line.prev_y = event.x, event.y
+        return
+    canvas.create_line(draw_line.prev_x, draw_line.prev_y, event.x, event.y, fill="black", width=2)
+    draw_line.prev_x, draw_line.prev_y = event.x, event.y
 
 window = Tk()
 canvas = Canvas(window)
 canvas.pack()
 canvas.bind("<B1-Motion>", paint)
+canvas.bind("<Button-3>", draw_line) 
 
 button_delete = Button(window, text="all clear", command=clear_paint)
 button_delete.pack()
 
 window.mainloop()
+
