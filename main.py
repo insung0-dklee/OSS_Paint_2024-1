6b67cae00f5474bb835d3d2d776561e4bb6faea1
DESKTOP-87NNJJ1\Administrator <zlorol6276@naver.com>
2024-05-30 18:56:25 +0900

    [기능추가] 브러쉬 크기 조절 기능

--- main.py
+++ main.py
@@ -10,13 +10,35 @@ window = Tk()
 canvas = Canvas(window)
 canvas.pack(fill=BOTH, expand=True)
 
+#브러쉬 사이즈 조절 기능 추가
+'''
+
+brush_size.set     : 기본값
+size.pack           : 툴바
+Scale	         : 크기를 결정짓는 변수
+
+'''
+
 brush_size = Scale(window, from_=1, to=20, orient=HORIZONTAL, label="Brush Size") 
 brush_size.set(5)  
 brush_size.pack() 
 
-'''
-scale      : 변수 1~20까지 조정
-set         : 기본값
-pack        : 크기를 조정하는 툴바
+#all clear 기능 추가
+"""
+Project : Paint
+paint : 내외부 검은색의 2픽셀 크기의 원을 이용해 그림을 그리는 기능
+clear_paint : 그림판에 있는 그림을 다 지우는 기능
+button_delete : clear_paint의 버튼
 
-'''
+"""
+
+def clear_paint():
+    canvas.delete("all")
+
+canvas.pack()
+canvas.bind("<B1-Motion>", paint)
+
+button_delete = Button(window, text="all clear", command=clear_paint)
+button_delete.pack()
+
+window.mainloop()
