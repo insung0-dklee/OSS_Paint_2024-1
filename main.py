633c2e3dca1492edaa9983458a73f4ac98ac8f22
YeLim Kang <yelim7433@naver.com>
2024-05-30 20:34:39 +0900

    [기능 추가] 그림 저장 기능 추가
    이 코드는 새로운 버튼인 그림 저장을 추가하고 이 버튼을 클릭하면 파일 저장 대화 상자가 열린다. 사용자가 파일 이름을 입력하면 그림이 해당 이름의 PNG 파일로 저장된다.

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ Project : Paint
 paint : 내외부 검은색의 2픽셀 크기의 원을 이용해 그림을 그리는 기능
 clear_paint : 그림판에 있는 그림을 다 지우는 기능
 button_delete : clear_paint의 버튼
+asksaveasfilename : 이름 입력시 해당이름의 PNG 파일로 그림 저장
+button_save : 파일 저장 대화 상자가 열리는 버튼
+
 
 """
 
 from tkinter import *
+from tkinter import filedialog
 
 def paint(event):
     x1, y1 = ( event.x-1 ), ( event.y-1 )
@@ -17,6 +21,14 @@ def paint(event):
 def clear_paint():
     canvas.delete("all")
 
+#그림 저장 기능 추가
+def save_image():
+    file = filedialog.asksaveasfilename(filetypes=[('PNG Images', '*.png')])
+    if file:
+        canvas.postscript(file=file + '.eps')
+        img = Image.open(file + '.eps')
+        img.save(file + '.png', 'png')
+
 window = Tk()
 canvas = Canvas(window)
 canvas.pack()
@@ -25,4 +37,10 @@ canvas.bind("<B1-Motion>", paint)
 button_delete = Button(window, text="all clear", command=clear_paint)
 button_delete.pack()
 
+button_save = Button(window, text="그림 저장", command=save_image)
+button_save.pack()
+
+
 window.mainloop()
+
+
