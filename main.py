09d1784c65a2cb4aa886dfab14c82f006cb187a6
TomGorani <int_kimty@naver.com>
2024-05-30 22:33:41 +0900

    [기능추가] 지우개 기능 추가

--- main.py
+++ main.py
@@ -13,22 +13,28 @@ drawing_mode = None
 start_x, start_y = None, None
 
 def paint(event):
-    x1, y1 = ( event.x-1 ), ( event.y-1 )
-    x2, y2 = ( event.x+1 ), ( event.y+1 )
-    canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
+    if drawing_mode == "draw":
+        x1, y1 = ( event.x-1 ), ( event.y-1 )
+        x2, y2 = ( event.x+1 ), ( event.y+1 )
+        canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
+    if drawing_mode == "erase":
+        x1, y1 = ( event.x-8 ), ( event.y-8 )
+        x2, y2 = ( event.x+8 ), ( event.y+8 )
+        canvas.create_rectangle(x1, y1, x2, y2, fill="white", outline="white")
 
 #all clear 기능 추가
 def clear_paint():
     canvas.delete("all")
-    
-    
-def draw_circle():
-    width = canvas.winfo_width()
-    height = canvas.winfo_height()
-    x1, y1 = (width // 2 - 50), (height // 2 - 50)
-    x2, y2 = (width // 2 + 50), (height // 2 + 50)
-    canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
 
+# 지우개 모드 활성화
+def activate_erase_mode():
+    global drawing_mode
+    drawing_mode = "erase"
+
+# 기본 그리기 모드 활성화
+def activate_draw_mode():
+    global drawing_mode
+    drawing_mode = "draw"
 
 # 마우스로 드래그하여 동그라미 그리기 기능
 def start_circle(event):
@@ -56,7 +62,6 @@ def activate_circle_mode():
     canvas.bind("<ButtonRelease-1>", end_circle)
   
 
-
 window = Tk()
 canvas = Canvas(window)
 canvas.pack()
@@ -66,10 +71,22 @@ canvas.bind("<B1-Motion>", paint)
 button_frame = Frame(window)
 button_frame.pack(side=TOP, fill=X)
 
-button_delete = Button(window, text="all clear", command=clear_paint)
-button_delete.pack()
+# all clear 버튼 추가
+button_delete = Button(button_frame, text="all clear", command=clear_paint)
+button_delete.pack(side=LEFT, padx=5, pady=5)
 
+# 그리기 모드 버튼 추가
+button_draw = Button(button_frame, text="draw", command=activate_draw_mode)
+button_draw.pack(side=LEFT, padx=5, pady=5)
+
+# 지우개 모드 버튼 추가
+button_erase = Button(button_frame, text="erase", command=activate_erase_mode)
+button_erase.pack(side=LEFT, padx=5, pady=5)
+
+# 동그라미 그리기 모드 추가
 button_circle = Button(button_frame, text="draw circle", command=activate_circle_mode)
 button_circle.pack()    
 
 window.mainloop()
+
+
