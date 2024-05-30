9a422eb8aa8fd0fc088ab93b52b407f4ae876760
JaeHo Kang <vibra79@yu.ac.kr>
2024-05-30 22:23:43 +0900

    [기능 추가] 전체 화면 전환 함수 추가.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Project : Paint
 paint : 내외부 검은색의 2픽셀 크기의 원을 이용해 그림을 그리는 기능
 clear_paint : 그림판에 있는 그림을 다 지우는 기능
 button_delete : clear_paint의 버튼
-
+full_screen : f11 입력을 통해 전체 화면으로 전환하는 기능
 """
 
 from tkinter import *
@@ -26,3 +26,20 @@ button_delete = Button(window, text="all clear", command=clear_paint)
 button_delete.pack()
 
 window.mainloop()
+
+# 함수는 F11 키 입력에 반응하여 전체 화면 모드를 전환합니다.
+def toggle_fullscreen(event):
+window.state = not window.state
+window.attributes("-fullscreen", window.state)
+
+window = Tk()
+window.state = False
+
+canvas = Canvas(window)
+canvas.pack()
+canvas.bind("<B1-Motion>", paint)
+
+button_delete = Button(window, text="all clear", command=clear_paint)
+button_delete.pack()
+
+window.bind("<F11>", toggle_fullscreen)
