081f7a05e8abcde6322093b5b67c3c88773999d5
JiYun Bang <zett0129@yu.ac.kr>
2024-05-30 18:03:44 +0900

    [기능추가] + 그림판 전체 지우기 기능

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+Project : Paint
+paint : 내외부 검은색의 2픽셀 크기의 원을 이용해 그림을 그리는 기능
+clear_paint : 그림판에 있는 그림을 다 지우는 기능
+button_delete : clear_paint의 버튼
+
+"""
+
 from tkinter import *
 
 def paint(event):
@@ -5,8 +13,16 @@ def paint(event):
     x2, y2 = ( event.x+1 ), ( event.y+1 )
     canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
 
+#all clear 기능 추가
+def clear_paint():
+    canvas.delete("all")
+
 window = Tk()
 canvas = Canvas(window)
 canvas.pack()
 canvas.bind("<B1-Motion>", paint)
+
+button_delete = Button(window, text="all clear", command=clear_paint)
+button_delete.pack()
+
 window.mainloop()
