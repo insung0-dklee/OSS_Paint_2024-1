0c7f9485ff957f2308d8d6f5b7842552244de4b4
PC12185\yanyo <hss85212002@gmail.com>
2024-05-30 18:48:56 +0900

    [기능 추가] 윈도우 창의 초기 설정과 사이즈 조절

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def paint(event):
     x2, y2 = ( event.x+1 ), ( event.y+1 )
     canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black")
 
+        
 window = Tk()
+window.geometry("640x400+100+100")
+#윈도우 창의 (너비)x(높이)+(초기 화면 위치의 x좌표)+(초기 화면 위치의 Y좌표)로
+#초기 윈도우 창의 크기와 위치를 설정
+window.resizable(True,True)
+#윈도우 창의 창 크기 조절여부를 (상하,좌우) 모두 True로 하여 윈도우 창의 크기를
+#조절할 수 있도록 함
 canvas = Canvas(window)
 canvas.pack()
 canvas.bind("<B1-Motion>", paint)
