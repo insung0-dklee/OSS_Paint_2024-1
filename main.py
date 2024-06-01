63c8bb3f9a70773032dc49a3a887fd68ab466e49
HwaYeon Kim <oouile@naver.com>
2024-06-01 23:59:05 +0900

    [기능추가] 세개의 선모양 브러쉬 추가

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ button_delete : clear_paint의 버튼
 """
 
 from tkinter import *
-import webbrowser
+import math
+
 
 def paint(event):
     x1, y1 = ( event.x-1 ), ( event.y-1 )
@@ -28,23 +29,14 @@ button_delete.pack()
 
 window.mainloop()
 
+# 세개의 선 모양의 브러쉬
+def draw_lines(event):
+    x1, y1 = (event.x - 20), (event.y - 20)
+    x2, y2 = (event.x + 20), (event.y + 20)
+    
+    # 세 개의 평행한 선 그리기
+    canvas.create_line(x1, y1, x2, y2, width=5, fill="black")
+    canvas.create_line(x1+10, y1, x2+10, y2, width=5, fill="black")
+    canvas.create_line(x1-10, y1, x2-10, y2, width=5, fill="black")
 
-# 사용자에게 입력을 받아 링크를 여는 함수
-def openLink():
-    link = link_entry.get()
-    webbrowser.open(link)
 
-root = Tk()  # 새로운 Tk 객체 생성
-canvas = Canvas(root, width=400, height=400)  # 캔버스 만들기
-canvas.pack()
-
-# 사용자에게 링크를 입력하라는 라벨을 캔버스에 만들기
-link_label = canvas.create_text(200, 150, text="Enter a link:", fill="hotpink", font=("Arial", 16))
-link_entry = Entry(root)
-canvas.create_window(200, 200, window=link_entry)
-
-# 클릭 시 openLink() 함수를 호출
-open_button = Button(root, text="Open Link", command=openLink)
-canvas.create_window(200, 250, window=open_button)
-
-root.mainloop()
