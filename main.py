6d8c656113893597b5f16a9a03317811a0c8ef81
SeEun <oseeun60@gmail.com>
2024-06-03 22:53:23 +0900

    [기능추가]사각별을 만드는 기능 추가

--- main.py
+++ main.py
@@ -1158,6 +1158,51 @@ def finish_diamond(event):
     if current_shape:
         canvas.itemconfig(current_shape, tags="")
 
+# 4각별을 생성하는 함수       
+def create_four_pointed_star(event=None):
+    select_shape_color()
+    canvas.bind("<Button-1>", start_four_pointed_star)
+
+# 4각별 모양 그릴 위치 정하고 생성하는 함수 호출
+def start_four_pointed_star(event):
+    global start_x, start_y, current_shape
+    start_x, start_y = event.x, event.y
+    current_shape = None
+    canvas.bind("<B1-Motion>", lambda event: draw_four_pointed_star(event))
+    canvas.bind("<ButtonRelease-1>", finish_four_pointed_star)
+
+# 4각별 모양 생성하기
+def draw_four_pointed_star(event):
+    global start_x, start_y, current_shape
+    canvas.delete("temp_shape")
+    outer_radius = ((start_x - event.x)**2 + (start_y - event.y)**2)**0.5
+    inner_radius = outer_radius / 2.0
+    points = []
+
+    for i in range(4):
+        angle_outer = math.radians(i * 90 - 90)
+        angle_inner = math.radians(i * 90 + 45 - 90)
+
+        x_outer = start_x + outer_radius * math.cos(angle_outer)
+        y_outer = start_y + outer_radius * math.sin(angle_outer)
+        x_inner = start_x + inner_radius * math.cos(angle_inner)
+        y_inner = start_y + inner_radius * math.sin(angle_inner)
+
+        points.append(x_outer)
+        points.append(y_outer)
+        points.append(x_inner)
+        points.append(y_inner)
+
+    current_shape = canvas.create_polygon(points, outline=shape_outline_color, fill=shape_fill_color, tags="temp_shape")
+
+# 4각별 모양 그리기 종료
+def finish_four_pointed_star(event):
+    global current_shape
+    canvas.unbind("<B1-Motion>")  
+    canvas.unbind("<ButtonRelease-1>")
+    if current_shape:
+        canvas.itemconfig(current_shape, tags="")
+
 #모양 선택하는 팝업 메뉴
 def choose_shape(event):
     popup = Menu(window, tearoff=0)
@@ -1169,6 +1214,7 @@ def choose_shape(event):
     popup.add_command(label="Heart", command=lambda: create_heart(event))
     popup.add_command(label="Cross", command=lambda: create_cross(event))
     popup.add_command(label="Diamond", command=lambda: create_diamond(event))
+    popup.add_command(label="Four Pointed Star", command=lambda: create_four_pointed_star(event))
     popup.post(event.x_root, event.y_root)  # 이벤트가 발생한 위치에 팝업 메뉴 표시
 
 
