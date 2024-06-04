adcfd43c15e7f1f487a228f5d1f7b2325ec63e0b
pyodolski <pjwp0928w@gmail.com>
2024-06-04 20:48:22 +0900

    [주석 추가]  TransformationHandler.py 주석 추가

--- TransformationHandler.py
+++ TransformationHandler.py
@@ -1,10 +1,23 @@
 from tkinter import Canvas
 
 class TransformationHandler:
+    """
+    TransformationHandler 클래스는 주어진 캔버스 객체의 도형들에 대해 다양한 변환 작업을 수행합니다.
+    """
+
     def __init__(self, canvas: Canvas):
+        """
+        TransformationHandler 객체를 초기화합니다.
+
+        매개변수:
+        canvas (Canvas): 변환 작업을 수행할 캔버스 객체.
+        """
         self.canvas = canvas
 
     def flip_horizontal(self):
+        """
+        캔버스에 그려진 모든 도형을 수평으로 반전시킵니다.
+        """
         objects = self.canvas.find_all()
         self.canvas.update()
         canvas_width = self.canvas.winfo_width()
@@ -16,6 +29,9 @@ class TransformationHandler:
             self.canvas.coords(obj, *coords)
 
     def flip_vertical(self):
+        """
+        캔버스에 그려진 모든 도형을 수직으로 반전시킵니다.
+        """
         objects = self.canvas.find_all()
         self.canvas.update()
         canvas_height = self.canvas.winfo_height()
@@ -27,6 +43,9 @@ class TransformationHandler:
             self.canvas.coords(obj, *coords)
 
     def rotate_90(self):
+        """
+        캔버스에 그려진 모든 도형을 시계 방향으로 90도 회전시킵니다.
+        """
         objects = self.canvas.find_all()
         self.canvas.update()
         canvas_width = self.canvas.winfo_width()
@@ -37,6 +56,7 @@ class TransformationHandler:
             for i in range(0, len(coords), 2):
                 x = coords[i]
                 y = coords[i + 1]
+                # 좌표를 시계 방향으로 90도 회전시킵니다.
                 new_x = y
                 new_y = canvas_width - x
                 new_coords.extend([new_x, new_y])
