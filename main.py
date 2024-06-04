fb893c371769a6c77429f79505d58013ce441e5b
d4eh0 <eogud3332@naver.com>
2024-06-04 18:18:17 +0900

    [기능추가] 메인화면에서 단축키가 할당된 버튼 위에 마우스 커서를 올리면 해당 버튼의 단축키 작게 팝업

--- main.py
+++ main.py
@@ -199,6 +199,49 @@ def on_enter(event):
 def on_leave(event):
     event.widget.config(bg="SystemButtonFace")
 
+class ToolTip(object):
+
+    def __init__(self, widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+
+    def showtip(self, text):
+        "Display text in tooltip window"
+        self.text = text
+        if self.tipwindow or not self.text:
+            return
+        x, y, cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() + 30
+        y = y + cy + self.widget.winfo_rooty() +20
+        self.tipwindow = tw = Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+        label = Label(tw, text=self.text, justify=CENTER,
+                      background="#ffffe0", relief=SOLID, borderwidth=1,
+                      font=("Arial", "8", "normal"))
+        label.grid(row=0, column=0, sticky="nsew")  # grid로 배치
+
+    def hidetip(self):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+
+def CreateToolTip(widget, text):
+    toolTip = ToolTip(widget)
+    def enter(event):
+        event.widget.config(bg="light blue")
+        toolTip.showtip(text)
+    def leave(event):
+        toolTip.hidetip()
+        event.widget.config(bg="SystemButtonFace")
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
+
+
+
 def upload_image():
     path = filedialog.askopenfilename()
     if path:
@@ -858,17 +901,20 @@ def setup_paint_app(window):
     button_erase_last_stroke.grid(row=2, column=1)
     button_erase_last_stroke.bind("<Enter>", on_enter)  # 마우스가 버튼 위에 올라갔을 때의 이벤트 핸들러 등록
     button_erase_last_stroke.bind("<Leave>", on_leave)  # 마우스가 버튼을 벗어났을 때의 이벤트 핸들러 등록
+    CreateToolTip(button_erase_last_stroke, text = '(Ctrl+Z)')
 
     button_redo_last_stroke = Button(labelframe_additional, text="Redo", command=rewrite_last_stroke)
     button_redo_last_stroke.grid(row=2, column=2)
     button_redo_last_stroke.bind("<Enter>", on_enter)  # 마우스가 버튼 위에 올라갔을 때의 이벤트 핸들러 등록
     button_redo_last_stroke.bind("<Leave>", on_leave)  # 마우스가 버튼을 벗어났을 때의 이벤트 핸들러 등록
+    CreateToolTip(button_redo_last_stroke, text = '(Ctrl+Y)')
 
     #all clear
     button_clear = Button(labelframe_additional, text="All Clear", command=lambda: clear_paint(canvas))
     button_clear.grid(row=1, column=3)
     button_clear.bind("<Enter>", on_enter)  # 마우스가 버튼 위에 올라갔을 때의 이벤트 핸들러 등록
     button_clear.bind("<Leave>", on_leave)  # 마우스가 버튼을 벗어났을 때의 이벤트 핸들러 등록
+    CreateToolTip(button_clear, text = '(C)')
 
     #도형 모양 선택하는 버튼 생성
     button_choose_shape = Button(labelframe_additional, text="shape", command=choose_shape)
